9a56d6e6a6ef258fcbd59fadad65519ad694b131
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-10-15 14:02:34 -0700

    make function

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const orders = {}
-const order = prompt(`Enter your Froyo order`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`);
-const arrOrder = order.split(`,`);
+const ordering = (arrOrder) => {
+  const orders = {}
 
-for (i = 0; i < arrOrder.length; i++) {
-  if (arrOrder[i] in orders) {
-    orders[arrOrder[i]] ++;
-  } 
-  else {
-    orders[arrOrder[i]] = 1;
-    }
-  console.log(orders)
+  for (i = 0; i < arrOrder.length; i++) {
+    if (arrOrder[i] in orders) {
+      orders[arrOrder[i]] ++;
+    } 
+    else {
+      orders[arrOrder[i]] = 1;
+      }
+  }
+  return orders
 }
-console.log(orders);
+const order = prompt(`Enter your Froyo order`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`);
+const froyos = order.split(`,`);
+console.log(ordering(froyos));
